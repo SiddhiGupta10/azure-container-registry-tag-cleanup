25c99e1c4b3136ab597dfe2524e2a6ff4e7a10fa
Siddhi Gupta <96565915+SiddhiGupta10@users.noreply.github.com>
2025-05-02 12:47:26 +0530

    Create deleteTags.ps1

---
+++ deleteTags.ps1
@@ -0,0 +1,47 @@
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string[]]$doNotDeleteTags,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string]$skipLastTags,
+
+    [Parameter(Mandatory = $true)]
+    [string]$registryName
+)
+
+try{
+    #Fetch the list of repositories in passed registry
+    $repoArray = az acr repository list --name $registryName -o json | ConvertFrom-Json
+
+    #To pass specific repository, name them in array
+    #$repoArray = ("jobs/azuresearchfunc","jobs/manageeventfunc","jobs/notificationfunc")
+
+    foreach ($repository in $repoArray)
+    {
+        $tagsArray = ( az acr repository show-tags --name $registryName --repository $repository --orderby time_asc -o json | ConvertFrom-Json ) | Select-Object -SkipLast $skipLastTags
+        
+        foreach($tag in $tagsArray) {
+            if ($donotdeletetags -Contains $tag) {
+                Write-Output " $($repository) : This tag is not deleted $($tag)"
+            } 
+            elseif ($donotdeletetags -NotContains $tag) {     
+                Write-Output "Repository $($repository): Deleting $($tag)"
+                az acr repository delete --name $registryName --image $repository":"$tag --yes
+            }
+        }
+    }
+}
+catch {
+    ShowError $_.Exception.Message
+}
+
+function ShowError {
+    $errorMessage = $args[0]
+    write-host "------------------------------------------------------------------"
+    Write-host "$errorMessage"
+    write-host "------------------------------------------------------------------"
+  }
